80c21f089eb6294a964c993aaba6a1f9845087bc
Saiteja Konda <saitejakonda@Saitejas-MacBook-Pro.local>
2021-04-25 17:12:56 +0530

    Initial commit

---
+++ src/main/java/com/konda/baskinnature/BaskinnatureApplication.java
@@ -0,0 +1,13 @@
+package com.konda.baskinnature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaskinnatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaskinnatureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/konda/baskinnature/service/services/ProductService.java
@@ -0,0 +1,15 @@
+package com.konda.baskinnature.service.services;
+
+import com.konda.baskinnature.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    Product addProduct(Product product);
+    List<Product> addProducts(List<Product> products);
+    Product updateProduct(Product product,String id);
+    List<Product> getProducts();
+    Product getProduct(String slug);
+    String deleteProduct(String id);
+    String deleteAllProduct();
+}
