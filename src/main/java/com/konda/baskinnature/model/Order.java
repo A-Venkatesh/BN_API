7becda1ebf2b783a06f92ddeb40f66da8e916a58
BaskInNature77 <82895982+BaskInNature77@users.noreply.github.com>
2021-06-08 19:07:37 +0530

    "mode internal"

--- src/main/java/com/konda/baskinnature/model/Order.java
+++ src/main/java/com/konda/baskinnature/model/Order.java
@@ -39,4 +39,5 @@ public class Order {
     private boolean couponApplied = false;
     private float discountedPrice;
     private Date postedOn = new Date();
+    private String mode = "internal";
 }
